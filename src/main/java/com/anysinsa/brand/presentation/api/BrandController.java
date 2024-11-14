c3686c2ac82630d9d6a0a6610bb9e08b02d9f69b
Park <junkipark430@gmai.com>
2024-11-14 20:21:35 +0900

    db logging 삭제 및 로깅

--- src/main/java/com/anysinsa/brand/presentation/api/BrandController.java
+++ src/main/java/com/anysinsa/brand/presentation/api/BrandController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.anysinsa.brand.application.dto.BrandResponseDTO;
 import io.micrometer.observation.annotation.Observed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,9 @@ import com.anysinsa.brand.domain.Brand;
 @Observed
 @RestController
 public class BrandController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BrandController.class);
+
     private final BrandService brandService;
 
     public BrandController(BrandService brandService) {
@@ -23,11 +28,13 @@ public class BrandController {
 
     @GetMapping(path = "/api/v1/brands")
     public ResponseEntity<List<Brand>> findBrands() {
+        logger.info("call BrandController::findBrands");
         return ResponseEntity.ok().body(brandService.findBrands());
     }
 
     @GetMapping(path = "/api/v1/brands/{id}")
     public ResponseEntity<BrandResponseDTO> findBrandById(@PathVariable Long id) {
+        logger.info("call BrandController::findBrandById::{}", id);
         return ResponseEntity.ok().body(brandService.findBrandById(id));
     }
 
