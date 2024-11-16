b49433ebc8775296d5a8a49bbaa53ae328350028
Park <junkipark430@gmai.com>
2024-11-17 01:39:24 +0900

    db도 tracing

--- src/main/java/com/anysinsa/common/config/OpenTelemetryConfig.java
+++ src/main/java/com/anysinsa/common/config/OpenTelemetryConfig.java
@@ -1,6 +1,17 @@
 package com.anysinsa.common.config;
 
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.extension.trace.propagation.B3Propagator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +20,33 @@ import org.springframework.context.annotation.Configuration;
 public class OpenTelemetryConfig {
 
     @Bean
-    public OtlpGrpcSpanExporter otlpGrpcSpanExporter(@Value("${management.otlp.tracing.endpoint}") String endpoint) {
-        return OtlpGrpcSpanExporter.builder()
-                .setEndpoint(endpoint)
+    public OpenTelemetry openTelemetry(@Value("${spring.application.name}") String serviceName) {
+        Resource resource = Resource.getDefault()
+                .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, serviceName)));
+
+        // JDBC 추적을 위한 SpanProcessor 추가
+        OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
+                .setEndpoint("http://localhost:4317")
                 .build();
+
+        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+                .addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build())
+                .setResource(resource)
+                .addSpanProcessor(SimpleSpanProcessor.create(spanExporter))  // 즉시 내보내기 위한 프로세서 추가
+                .build();
+
+        OpenTelemetrySdk sdk = OpenTelemetrySdk.builder()
+                .setTracerProvider(sdkTracerProvider)
+                .setPropagators(ContextPropagators.create(B3Propagator.injectingMultiHeaders()))
+                .buildAndRegisterGlobal();
+
+        // JDBC 추적을 위한 Tracer 설정
+        sdk.getTracer("jdbc")
+                .spanBuilder("jdbc-operations")
+                .setSpanKind(SpanKind.CLIENT)
+                .startSpan()
+                .end();
+
+        return sdk;
     }
 }
