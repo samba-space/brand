04f6fcfaf4b6f95884b0b3b11e5ec0df4efa5c0c
Park <junkipark430@gmai.com>
2024-11-17 17:25:53 +0900

    opentelemetryconfig 수정

--- src/main/java/com/anysinsa/common/config/OpenTelemetryConfig.java
+++ src/main/java/com/anysinsa/common/config/OpenTelemetryConfig.java
@@ -2,13 +2,11 @@ package com.anysinsa.common.config;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.extension.trace.propagation.B3Propagator;
@@ -24,7 +22,6 @@ public class OpenTelemetryConfig {
         Resource resource = Resource.getDefault()
                 .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, serviceName)));
 
-        // JDBC 추적을 위한 SpanProcessor 추가
         OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
                 .setEndpoint("http://localhost:4317")
                 .build();
@@ -32,21 +29,11 @@ public class OpenTelemetryConfig {
         SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
                 .addSpanProcessor(BatchSpanProcessor.builder(spanExporter).build())
                 .setResource(resource)
-                .addSpanProcessor(SimpleSpanProcessor.create(spanExporter))  // 즉시 내보내기 위한 프로세서 추가
                 .build();
 
-        OpenTelemetrySdk sdk = OpenTelemetrySdk.builder()
+        return OpenTelemetrySdk.builder()
                 .setTracerProvider(sdkTracerProvider)
                 .setPropagators(ContextPropagators.create(B3Propagator.injectingMultiHeaders()))
                 .buildAndRegisterGlobal();
-
-        // JDBC 추적을 위한 Tracer 설정
-        sdk.getTracer("jdbc")
-                .spanBuilder("jdbc-operations")
-                .setSpanKind(SpanKind.CLIENT)
-                .startSpan()
-                .end();
-
-        return sdk;
     }
 }
